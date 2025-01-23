5725ebbc4b1a83b47ddc278427e08f504b61fe87
Mats <mats@dune.com>
2025-01-23 15:19:53 +0100

    adapt to dune

--- README.md
+++ README.md
@@ -1,42 +1,29 @@
-# Helius RPC Proxy
+# Dune Echo Proxy
 
-[![RPC Proxy](docs/rpc_proxy.png)](https://helius.xyz)
-
-This repo hosts a one-click-deploy Cloudflare worker that proxies RPC requests to Helius. The proxy will allow you to keep your API key
-hidden from public requests made by clients. You will need both a [Helius](https://helius.xyz) account and a [Cloudflare](https://cloudflare.com) account to deploy this. Helius offers 100k credits for free each month, and Cloudflare workers can execute 100k invocations each day for free. Most projects can easily get started within these free tiers.
-
-Both standard JSON RPC and Websockets are supported!
-
-[Video Walkthrough](https://www.loom.com/share/a7add579f1c349d2a4bcab96ee04c47e)
+This repo hosts a one-click-deploy Cloudflare worker that proxies requests to Dune Echo. The proxy will allow you to keep your API key
+hidden from public requests made by clients. You will need both a [Dune](https://dune.com) account and a [Cloudflare](https://cloudflare.com) account to deploy this. Dune offers 10RPS for free, and Cloudflare workers can execute 100k invocations each day for free. Most projects can easily get started within these free tiers.
 
 # Setup
 ### Step 1
 
 Press the button below to deploy this to your own Cloudflare account:
 
-[![Deploy to Cloudflare Workers](https://deploy.workers.cloudflare.com/button)](https://deploy.workers.cloudflare.com/?url=https://github.com/helius-labs/helius-rpc-proxy)
+[![Deploy to Cloudflare Workers](https://deploy.workers.cloudflare.com/button)](https://deploy.workers.cloudflare.com/?url=https://github.com/duneanalytics/echo-proxy)
 
 ### Step 2
 
 Navigate to your newly deployed worker, and click "Settings" and then "Variables":
 
-![Variables](docs/add_variable.png)
-
 ### Step 3
-Add a new variable with the key name `HELIUS_API_KEY` and your Helius API key as the value:
-
-![Add Secret](docs/add_secret.png)
+Add a new variable with the key name `DUNE_API_KEY` and your Dune API key as the value:
 
 > NOTE: We recommend selecting "Encrypt". This will hide your key from the UI and API responses, and redact them from logs.
-
-![Encrypt](docs/encrypt.png)
 
 ### Step 4
 Refresh the page and confirm that your key is now saved and encrypted:
 
-![Confirm](docs/confirm.png)
-
 You can now use your worker URL as an the RPC endpoint in all SDK and client side configurations without your API key leaking!
+
 # Additional Security Steps
 This implementation is intentionally left in a less-than-ideal security state to facilitate easy deployment by anyone. If you would like to
 lock down your RPC proxy further, consider the following steps after you have successfully deployed the worker:
--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,52 @@
 interface Env {
 	CORS_ALLOW_ORIGIN: string;
-	HELIUS_API_KEY: string;
+	DUNE_API_KEY: string;
 }
 
 export default {
 	async fetch(request: Request, env: Env) {
 
 		// If the request is an OPTIONS request, return a 200 response with permissive CORS headers
-		// This is required for the Helius RPC Proxy to work from the browser and arbitrary origins
-		// If you wish to restrict the origins that can access your Helius RPC Proxy, you can do so by
+		// This is required for the Dune RPC Proxy to work from the browser and arbitrary origins
+		// If you wish to restrict the origins that can access your Dune RPC Proxy, you can do so by
 		// changing the `*` in the `Access-Control-Allow-Origin` header to a specific origin.
 		// For example, if you wanted to allow requests from `https://example.com`, you would change the
 		// header to `https://example.com`. Multiple domains are supported by verifying that the request
 		// originated from one of the domains in the `CORS_ALLOW_ORIGIN` environment variable.
-		const supportedDomains = env.CORS_ALLOW_ORIGIN ? env.CORS_ALLOW_ORIGIN.split(',') : undefined;
-		const corsHeaders: Record<string, string> = {
+		const supportedDomains = env.CORS_ALLOW_ORIGIN?.split(',');
+		const headers = {
 			"Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, OPTIONS",
 			"Access-Control-Allow-Headers": "*",
-		}
-		if (supportedDomains) {
-			const origin = request.headers.get('Origin')
-			if (origin && supportedDomains.includes(origin)) {
-				corsHeaders['Access-Control-Allow-Origin'] = origin
-			}
-		} else {
-			corsHeaders['Access-Control-Allow-Origin'] = '*'
-		}
+			"Access-Control-Allow-Origin": supportedDomains 
+				? request.headers.get('Origin')
+					? supportedDomains.includes(request.headers.get('Origin')!)
+						? request.headers.get('Origin')!
+						: undefined
+					: undefined
+				: '*'
+		};
 
 		if (request.method === "OPTIONS") {
 			return new Response(null, {
 				status: 200,
-				headers: corsHeaders,
+				headers: headers as Record<string, string>,
 			});
 		}
 
-		const upgradeHeader = request.headers.get('Upgrade')
+		const url = new URL(request.url);
 
-		if (upgradeHeader || upgradeHeader === 'websocket') {
-			return await fetch(`https://mainnet.helius-rpc.com/?api-key=${env.HELIUS_API_KEY}`, request)
-		}
-
-
-		const { pathname, search } = new URL(request.url)
-		const payload = await request.text();
-		const proxyRequest = new Request(`https://${pathname === '/' ? 'mainnet.helius-rpc.com' : 'api.helius.xyz'}${pathname}?api-key=${env.HELIUS_API_KEY}${search ? `&${search.slice(1)}` : ''}`, {
-			method: request.method,
-			body: payload || null,
-			headers: {
-				'Content-Type': 'application/json',
-				'X-Helius-Cloudflare-Proxy': 'true',
+		// Clone the request to modify headers
+		const req = new Request(
+			`https://api.dune.com${url.pathname}${url.search}`,
+			{
+				...request,
+				headers: new Headers({
+					...Object.fromEntries(request.headers.entries()),
+					'x-dune-api-key': env.DUNE_API_KEY
+				})
 			}
-		});
+		);
 
-		return await fetch(proxyRequest).then(res => {
-			return new Response(res.body, {
-				status: res.status,
-				headers: corsHeaders
-			});
-		});
+		return fetch(req);
 	},
 };
